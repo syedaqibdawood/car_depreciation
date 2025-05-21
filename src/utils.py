1fd176d2309cd46531905bb31bd0fcd240a27530
Aqib <syedaqibdawood@gmail.com>
2025-05-20 20:05:07 -0400

    replaced preprocessor from sagemaker

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
+import joblib
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -20,6 +21,13 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return joblib.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
 
@@ -50,14 +58,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-
-    except Exception as e:
-        raise CustomException(e, sys)
-
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
